ffc935633ab2352c98236ad5e8f10716e6588ce3
fnxiang <654879744@qq.com>
2021-05-02 17:05:26 +0800

    update send file method

--- src/main/java/org/example/ReadFile.java
+++ src/main/java/org/example/ReadFile.java
@@ -27,8 +27,8 @@ class ReadFile {
     // send the filename to the server
     DataOutputStream dos = new DataOutputStream(s.getOutputStream()); //写Socket的输出流
     dos.writeUTF(fileName);
+    receiveFile(s, "example.txt");
     dos.flush();
-    dos.close();
   }
 
   // This method will receive the file from the Server,
@@ -39,20 +39,24 @@ class ReadFile {
 
     // FILL IN THIS METHOD - Hint: look at the
     // simpleClient code
-    InputStream input = s.getInputStream();
-    DataInputStream inputStream  = new DataInputStream(input);
-    DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(file_path));
-    byte[] buf = new byte[1027*9];
-    int len = 0;
-    while((len=inputStream.read(buf))!=-1){
-      outputStream.write(buf, 0, len);
+    DataInputStream inputStream=null;
+    DataOutputStream outputStream=null;
+    try{
+      inputStream  = new DataInputStream(new BufferedInputStream(s.getInputStream()));
+      outputStream = new DataOutputStream(new FileOutputStream(file_path));
+      byte[] buf = new byte[100];
+      int len = 0;
+      while((len=inputStream.read(buf))!=-1){
+        outputStream.write(buf, 0, len);
+      }
+      outputStream.flush();
+      System.out.println("file received!");
+    }catch (Exception e){
+      System.out.println(e.getMessage());
+    }finally {
+      inputStream.close();
+      outputStream.close();
     }
-
-    outputStream.flush();
-    System.out.println("file received!");
-
-    inputStream.close();
-    outputStream.close();
   }
 
   public static void main(String args[]) {
@@ -71,6 +75,7 @@ class ReadFile {
       // Open our connection to args[0]
       s = new Socket(args[0], port);
       // Send the file name to the Server
+      System.out.println("connect success！");
       sendFileName (s, args[1]);
 
       // Output the file (or result of the request)
